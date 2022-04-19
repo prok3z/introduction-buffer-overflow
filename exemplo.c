dfbfb5e93899d681e40114c0eff2d6d2894c6178
dock0d1 <43358190+ryp3@users.noreply.github.com>
2022-04-19 10:39:43 -0300

    Exemplo 1 sobre BoF

---
+++ exemplo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    char ex1[] = "rip3"; // conteudo
+    char ex2[5]; // vetor 2 vazio
+
+// obter input do usuario para colocar no vetor 2
+    scanf("%s", ex2); 
+
+// mostra o conteudo dos 2 vetores
+
+    printf("ex1 = %s\n", ex1); 
+    printf("ex2 = %s\n", ex2);
+    
+    return 0;
+}
